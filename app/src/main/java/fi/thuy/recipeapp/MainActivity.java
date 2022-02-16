85594ba4b6dc6d7359276c629435bb757df71437
prabind <prabin.dhakal@metropolia.fi>
2022-02-16 12:24:00 +0200

    added icons,  horizontal scroll view

--- app/src/main/java/fi/thuy/recipeapp/MainActivity.java
+++ app/src/main/java/fi/thuy/recipeapp/MainActivity.java
@@ -1,14 +1,34 @@
 package fi.thuy.recipeapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String TAG = "MAINACTIVITY";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button btnStart = findViewById(R.id.buttonGetStarted);
+
+        btnStart.setOnClickListener((view) -> handleStartClick());
+
+    }
+
+    private void handleStartClick(){
+        Log.d(TAG,"Get Started clicked");
+        Intent intent = new Intent(this, StartActivity.class);
+        startActivity(intent);
     }
 }
