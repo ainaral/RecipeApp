c5c67df0d039b870bde609ed83e82c0e31221dfa
prabind <prabin.dhakal@metropolia.fi>
2022-02-17 02:11:17 +0200

    minor changes in layout

--- app/src/main/java/fi/thuy/recipeapp/StartActivity.java
+++ app/src/main/java/fi/thuy/recipeapp/StartActivity.java
@@ -3,6 +3,8 @@ package fi.thuy.recipeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +12,28 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button btnBreakfast = findViewById(R.id.buttonBreakfast);
+        Button btnLunch = findViewById(R.id.buttonLunch);
+        Button btnDinner = findViewById(R.id.buttonDinner);
+
+        MyClick myClick = new MyClick();
+
+        btnBreakfast.setOnClickListener(myClick);
+        btnLunch.setOnClickListener(myClick);
+        btnDinner.setOnClickListener(myClick);
+    }
+
+
+    private class MyClick implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            updateUI(view);
+        }
+    }
+
+    private void updateUI(View view){
+
     }
 }
