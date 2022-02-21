c6a7acaf7a09092bd8bc32d459537c128c4518ad
prabind <prabin.dhakal@metropolia.fi>
2022-02-21 14:19:36 +0200

    added recyclerView for Recipe

--- app/src/main/java/fi/thuy/recipeapp/StartActivity.java
+++ app/src/main/java/fi/thuy/recipeapp/StartActivity.java
@@ -2,6 +2,7 @@ package fi.thuy.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,12 +17,14 @@ public class StartActivity extends AppCompatActivity {
         Button btnBreakfast = findViewById(R.id.buttonBreakfast);
         Button btnLunch = findViewById(R.id.buttonLunch);
         Button btnDinner = findViewById(R.id.buttonDinner);
+        Button btnMore = findViewById(R.id.buttonMore);
 
         MyClick myClick = new MyClick();
 
         btnBreakfast.setOnClickListener(myClick);
         btnLunch.setOnClickListener(myClick);
         btnDinner.setOnClickListener(myClick);
+        btnMore.setOnClickListener(myClick);
     }
 
 
@@ -34,6 +37,20 @@ public class StartActivity extends AppCompatActivity {
     }
 
     private void updateUI(View view){
+        Intent intent = new Intent(this, ThirdActivity.class);
 
+        if(view.getId() == R.id.buttonBreakfast){
+            intent.putExtra("key", "Breakfast" );
+
+        }else if(view.getId() == R.id.buttonLunch){
+            intent.putExtra("key", "Lunch");
+
+        }else if(view.getId() == R.id.buttonDinner){
+            intent.putExtra("key","Dinner");
+        }else{
+            intent.putExtra("key","More");
+        }
+
+        startActivity(intent);
     }
 }
--- app/src/main/java/fi/thuy/recipeapp/ThirdActivity.java
+++ app/src/main/java/fi/thuy/recipeapp/ThirdActivity.java
@@ -1,14 +1,93 @@
 package fi.thuy.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import fi.thuy.recipecontents.Recipe;
+import fi.thuy.recipecontents.RecipeList;
+import fi.thuy.recipecontents.RecipeListAdapter;
 
 public class ThirdActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    RecipeList recipes = RecipeList.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Field[] drawables = R.drawable.class.getDeclaredFields();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra( "key");
+
+        recyclerView = findViewById(R.id .recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        try {
+            JSONObject jsonObject = new JSONObject((fetchJsonData()));
+            JSONArray jsonArray = jsonObject.getJSONArray("recipesList");
+
+            for(int i = 0; i < jsonArray.length(); i++) {
+
+                Log.d("Message", message);
+                JSONObject listOfRecipe = jsonArray.getJSONObject(i);
+                String recipeTitle = listOfRecipe.getString("title");
+                String mealType = listOfRecipe.getString("mealtype");
+                String instructions = listOfRecipe.getString("instructions");
+                String ingredients = listOfRecipe.getString("ingredients");
+                String tags = listOfRecipe.getString("tags");
+
+                if (message.equals(mealType)) {
+                    recipes.addRecipe(new Recipe(i, recipeTitle, mealType, instructions, ingredients, tags));
+
+                }
+            }
+            Log.d("Key", message);
+
+            }catch (Exception e){
+                e.printStackTrace();
+        }
+
+
+        RecipeListAdapter recipeListAdapter = new RecipeListAdapter(ThirdActivity.this, recipes.getListOfRecipes());
+        recyclerView.setAdapter(recipeListAdapter);
+
+    }
+
+
+
+    private String fetchJsonData() {
+        recipes.clear();
+        String json;
+        try {
+            InputStream inputStream = getAssets().open("recipeList.json");
+            int sizeOfFile = inputStream.available();
+            byte[] bufferData = new byte[sizeOfFile];
+            inputStream.read(bufferData);
+            inputStream.close();
+            json = new String(bufferData,"UTF-8");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return  null;
+        }
+        return json;
     }
 }
