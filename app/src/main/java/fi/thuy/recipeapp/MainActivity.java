d8d0e029980ca60ef66b1db0f7d1fde803daa43f
Ainara Larrañaga <ainara.larranagaflores@metropolia.fi>
2022-03-09 11:07:07 +0200

    "Hid action bar"

--- app/src/main/java/fi/thuy/recipeapp/MainActivity.java
+++ app/src/main/java/fi/thuy/recipeapp/MainActivity.java
@@ -5,11 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Objects;
 
 /**
  * This is a main method which is very important
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
         move();
 
